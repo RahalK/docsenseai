4ba122302d2b934afd9de1f89486a6f74313cfea
Karen RAHAL <karen.rahal2@gmail.com>
2025-12-01 14:16:21 +0100

    Update

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 st.set_page_config(page_title="AI Doc & Image Analyzer", layout="wide")
 
-st.markdown("<h1 style='text-align: center; color: #4B0082;'>✨ AI Document & Image Analyzer</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: #4B0082;'>AI Document & Image Analyzer</h1>", unsafe_allow_html=True)
+
 st.markdown("""
 Welcome to your AI Document & Image Analyzer!
 
@@ -14,4 +15,26 @@ Use the sidebar or the pages below to:
 st.info("""
 Make sure your **OpenAI API key** is stored in a `.env` file:
 OPENAI_API_KEY=your_key_here
-        """)
+""")
+
+# import streamlit as st
+
+# st.set_page_config(page_title="AI Doc & Image Analyzer", layout="wide")
+
+# st.markdown(
+#     f"<div style='background-color:#F3E8FF; color:#2C2C2C; padding:15px; border-radius:10px;'>{summary}</div>",
+#     unsafe_allow_html=True
+# )
+# st.markdown("<h1 style='text-align: center; color: #4B0082;'>✨ AI Document & Image Analyzer</h1>", unsafe_allow_html=True)
+# st.markdown("""
+# Welcome to your AI Document & Image Analyzer!
+
+# Use the sidebar or the pages below to:
+# - Summarize PDFs quickly and clearly.
+# - Generate detailed descriptions of uploaded images.
+# """)
+
+# st.info("""
+# Make sure your **OpenAI API key** is stored in a `.env` file:
+# OPENAI_API_KEY=your_key_here
+#         """)
